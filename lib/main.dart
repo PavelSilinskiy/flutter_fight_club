3fecfb5527c554bd56e57ed79b5964dcbb23ba4a
PavelSilinskiy <pavel.silinskiy.03@mail.ru>
2026-01-22 14:09:51 +0300

    Applied font, removed colorScheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -6,29 +8,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: GoogleFonts.pressStart2pTextTheme(textTheme),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -73,27 +62,31 @@ class _MyHomePageState extends State<MyHomePage> {
           Row(
             children: [
               SizedBox(width: 16),
-              Expanded(child: Column(
-                children: [
-                  Center(child: Text('You'),),
-                  Text('1'),
-                  Text('1'),
-                  Text('1'),
-                  Text('1'),
-                  Text('1'),
-                ],
-              )),
+              Expanded(
+                child: Column(
+                  children: [
+                    Center(child: Text('You')),
+                    Text('1'),
+                    Text('1'),
+                    Text('1'),
+                    Text('1'),
+                    Text('1'),
+                  ],
+                ),
+              ),
               SizedBox(width: 12),
-              Expanded(child: Column(
-                children: [
-                  Center(child: Text('Enemy'),),
-                  Text('1'),
-                  Text('1'),
-                  Text('1'),
-                  Text('1'),
-                  Text('1'),
-                ],
-              )),
+              Expanded(
+                child: Column(
+                  children: [
+                    Center(child: Text('Enemy')),
+                    Text('1'),
+                    Text('1'),
+                    Text('1'),
+                    Text('1'),
+                    Text('1'),
+                  ],
+                ),
+              ),
               SizedBox(width: 16),
             ],
           ),

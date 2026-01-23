cf411f2df7723a0d3c2fb0d5ed9e1e3dcae55a95
PavelSilinskiy <pavel.silinskiy.03@mail.ru>
2026-01-23 14:29:19 +0300

    fixed images paths

--- lib/fight_club_images.dart
+++ lib/fight_club_images.dart
@@ -2,8 +2,8 @@ class FightClubImages {
 
   FightClubImages._();
 
-  static const enemyAvatar = 'assets/icons/enemy-avatar.png';
-  static const youAvatar = 'assets/icons/you-avatar.png';
+  static const enemyAvatar = 'assets/images/enemy-avatar.png';
+  static const youAvatar = 'assets/images/you-avatar.png';
    
   
 }

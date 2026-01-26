b9d96c6358b17eec6d2670fc4616395a3e817100
PavelSilinskiy <pavel.silinskiy.03@mail.ru>
2026-01-26 05:39:11 +0300

    feature: Mark up the MainPage

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fight_club/fight_club_colors.dart';
+import 'package:flutter_fight_club/go_button.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return _MainPageContent();
   }
 }
 
@@ -15,7 +16,44 @@ class _MainPageContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: FightClubColors.background,
-      body: SafeArea(child: Text('whnefiqne')),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              //left: 16,
+              //right: 16,
+              top: 24,
+              bottom: 16,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'The\nfight\nclub'.toUpperCase(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: FightClubColors.darkGreyText,
+                    fontSize: 30,
+                  ),
+                ),
+                Expanded(child: SizedBox()),
+                GoButton(
+                  onTap: () {},
+                  color: FightClubColors.blackButton,
+                  text: 'Start'.toUpperCase(),
+                ),
+                SizedBox(height: 12),
+                GoButton(
+                  onTap: () {},
+                  color: FightClubColors.blackButton,
+                  text: 'Start'.toUpperCase(),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
     // TODO: implement build
     throw UnimplementedError();

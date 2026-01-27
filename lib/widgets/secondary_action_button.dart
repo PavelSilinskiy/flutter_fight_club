82201ffcc12556839a9de04c6cb14ae074146fdf
PavelSilinskiy <pavel.silinskiy.03@mail.ru>
2026-01-27 03:09:37 +0300

    feature: create secondary action button

---
+++ lib/widgets/secondary_action_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fight_club/resources/fight_club_colors.dart';
+
+class SecondaryActionButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+
+  const SecondaryActionButton({
+    super.key,
+    required this.onTap,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 40,
+        width: double.infinity,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: FightClubColors.transparentButton,
+          border: Border.all(
+            width: 2,
+            color: FightClubColors.darkGreyText,
+          ),
+        ),
+        child: Text(
+          text.toUpperCase(),
+          style: TextStyle(
+            color: FightClubColors.darkGreyText,
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+    );
+  }
+}

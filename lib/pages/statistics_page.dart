a48199244ee2d67908fdd864a1289378423f20c9
PavelSilinskiy <pavel.silinskiy.03@mail.ru>
2026-01-27 20:08:14 +0300

    feature: Make statistics display

--- lib/pages/statistics_page.dart
+++ lib/pages/statistics_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fight_club/resources/fight_club_colors.dart';
 import 'package:flutter_fight_club/widgets/secondary_action_button.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class StatisticsPage extends StatelessWidget {
   const StatisticsPage({super.key});
@@ -14,6 +15,9 @@ class StatisticsPage extends StatelessWidget {
 class _StatisticsPageContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int? wonCount;
+    int? drawCount;
+    int? lostCount;
     return Scaffold(
       backgroundColor: FightClubColors.background,
       body: SafeArea(
@@ -30,7 +34,56 @@ class _StatisticsPageContent extends StatelessWidget {
                   ),
                 ),
               ),
-              Expanded(child: SizedBox()),
+              Expanded(
+                child: Center(
+                  child: SizedBox(
+                    height: 132,
+                    child: FutureBuilder(
+                      future: SharedPreferences.getInstance().then((
+                        sharedPreferences,
+                      ) {
+                        wonCount = sharedPreferences.getInt('stats_won');
+                        drawCount = sharedPreferences.getInt('stats_draw');
+                        lostCount = sharedPreferences.getInt('stats_lost');
+                      }),
+                      builder: (context, asyncSnapshot) {
+                        if (asyncSnapshot.connectionState ==
+                            ConnectionState.done) {
+                          return Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text(
+                                'Won: ${wonCount ?? 0}',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: FightClubColors.darkGreyText,
+                                ),
+                              ),
+                              Text(
+                                'Draw: ${drawCount ?? 0}',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: FightClubColors.darkGreyText,
+                                ),
+                              ),
+
+                              Text(
+                                'Lost: ${lostCount ?? 0}',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: FightClubColors.darkGreyText,
+                                ),
+                              ),
+                            ],
+                          );
+                        } else {
+                          return SizedBox();
+                        }
+                      },
+                    ),
+                  ),
+                ),
+              ),
               Padding(
                 padding: const EdgeInsets.only(bottom: 16.0),
                 child: SecondaryActionButton(

84a5531fb33cbecb7e4a2a658b7138959c9a1ea1
PavelSilinskiy <pavel.silinskiy.03@mail.ru>
2026-01-27 03:30:11 +0300

    test: tasks 1 and 2 complete

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@ import 'lesson_3/task_4.dart';
 
 void main() {
   group("l04h01", () => runTestLesson3Task1());
-  // group("l04h02", () => runTestLesson3Task2());
+  group("l04h02", () => runTestLesson3Task2());
   // group("l04h03", () => runTestLesson3Task3());
   // group("l04h04", () => runTestLesson3Task4());
 }
